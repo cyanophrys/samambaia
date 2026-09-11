f23260b1e2d490885ac8fdb4be6b73bf725fc2f8
Raul Sousa <cyanophrys@tuta.io>
2026-09-11 14:06:23 -0300

    feat: add smb-heading-group

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,7 @@
 
 import '../components/smb-alert-dialog/smb-alert-dialog.js';
 import '../components/smb-dialog/smb-dialog.js';
+import '../components/smb-heading-group/smb-heading-group.js';
 import '../components/smb-search/smb-search.js';
 import '../components/smb-stack/smb-stack.js';
 import '../components/smb-toast/smb-toast.js';
