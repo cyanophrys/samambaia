c76f8638663f201f2d2da98ab34aa02ebe727363
Raul Sousa <cyanophrys@tuta.io>
2026-09-17 19:47:31 -0300

    refactor(build): simplify build now that manifest lives in src

--- build.js
+++ build.js
@@ -1,9 +1,7 @@
 import {
   cp,
   mkdir,
-  readFile,
   rm,
-  writeFile,
 } from 'node:fs/promises';
 
 import path from 'node:path';
@@ -17,25 +15,4 @@ await mkdir(dist, { recursive: true });
 
 await cp(src, dist, {
   recursive: true,
-  filter: (source) => path.basename(source) !== 'manifest.json',
 });
-
-const manifest = JSON.parse(
-  await readFile(
-    path.join(src, 'manifest.json'),
-    'utf8'
-  )
-);
-
-for (const size of Object.keys(manifest.icons)) {
-  manifest.icons[size] =
-    manifest.icons[size].replace(/^src\//, '');
-}
-
-manifest.background.service_worker =
-  manifest.background.service_worker.replace(/^src\//, '');
-
-await writeFile(
-  path.join(dist, 'manifest.json'),
-  JSON.stringify(manifest, null, 2) + '\n'
-);
