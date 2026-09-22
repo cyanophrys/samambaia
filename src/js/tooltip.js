9f637e0a6a5d585bfeceeb3d2b29deb80a746659
Raul Sousa <cyanophrys@tuta.io>
2026-09-21 22:40:15 -0300

    refactor(tooltip): use an ID for the tooltip div
    This was only a class because it lived in common.css before.
    But now that components use the merged styles.css file this
    doesn't matter anymore.

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -15,7 +15,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-const tooltip = document.querySelector('.tooltip');
+const tooltip = document.getElementById('tooltip');
 
 const SHOW_DELAY = 500;
 
